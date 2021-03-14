a36d5bb3eec8376b3c222ea0aa984599cb5ffc65
Carlos-I <panfletoindiegames@gmail.com>
2021-03-14 11:15:12 -0600

    Delete Módulo2 directory

--- Módulo2/semana1/clase1/programa1.js
+++
@@ -1,9 +0,0 @@
-//Programa 1 en JavaScript
-//variable cambiable
-var anio = 365;
-console.log(anio);
-
-
-//Mensaje constante
-const mensaje= "Bienvenido a Ciudad Rio Verde";
-console.log(mensaje);
--- Módulo2/semana1/clase2/carro.js
+++
@@ -1,5 +0,0 @@
-var placa = "HHF-453";
-const modelo = "Corolla";
-const marca = "Toyota";
-const paisOrigen = "Japón";
-const anio = 2010; 
