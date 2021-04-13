ba3fecf0b26699839400ac023fc377095cfa03a0
PanfletoIG <panfletoindiegames@gmail.com>
2021-04-13 14:27:58 -0600

    1.13/04/2021

---
+++ Módulo3/semana3/api.js
@@ -0,0 +1,19 @@
+const https = require('https');
+
+https.get('https://www.omdbapi.com/?apikey=8cfa1544&t=gotham', 
+(respuesta) => {
+    let informacion = '';
+
+    respuesta.on("data", 
+    (chunk)=>{
+        informacion += chunk;
+    });
+
+    respuesta.on('end', 
+    () => {
+        console.log(informacion);
+    })
+}).on('error', 
+    (error) => {
+        console.log("Error: " + error);
+});
