5aad7cd4fcd5bf0e06096e978b7e21080ca50907
PanfletoIG <panfletoindiegames@gmail.com>
2021-04-15 14:16:05 -0600

    actualizacion 1.15/04/2021

--- Módulo3/semana3/sevidor.js
+++ Módulo3/semana3/sevidor.js
@@ -3,9 +3,24 @@ const http = require('http');
 const host = 'localhost';
 const port = 8080;
 
+//servicio
+//mensaje que va a dar
 const servicioMs = function(peticion, respuesta)
 {
-    respuesta.setHeader("Content-Type", "application/JSON");
+    respuesta.setHeader("Content-Type", "application/json");
     respuesta.writeHead(200);
-    respuesta.end({mensaje : "Hola Mundo"});
+    respuesta.end(JSON.stringify({
+        mensaje : "Hola Mundo",
+        Oa: "Oa"
+    }));
 };
+
+
+//servidor
+//creador servidor
+const servidor = http.createServer(servicioMs);
+
+servidor.listen(port, host, 
+    () => {
+        console.log("Servidor prendido")
+});
