c68aec7645eb7abbb76b58f012cb8fe18a0cb4c6
PanfletoIG <panfletoindiegames@gmail.com>
2021-04-17 08:54:52 -0600

    actualizacion 1.15/04/2021

--- Módulo3/semana3/peticionS.js
+++ Módulo3/semana3/peticionS.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 var info;
-axios.get('http://localhost:8080')
+axios.get('http://localhost:8080/saludo')
 .then(respuesta => {
     info = respuesta.data;
     console.log(info);
