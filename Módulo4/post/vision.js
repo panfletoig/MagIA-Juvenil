7ad8df14c9d9d9c766a174d106098a670fa578cd
PanfletoIG <panfletoindiegames@gmail.com>
2021-04-27 14:13:00 -0600

    actualizacion 1.27/04/2021

---
+++ Módulo4/post/vision.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+
+//guardamos el Json en una variable
+var url = {url: "https://kartox.com/media/catalog/product/cache/1/image/940x940/9df78eab33525d08d6e5fb8d27136e95/k/a/kartox_caja_con_solapas_2.jpg"};
+
+//direccion de la peticion {endpoint/punto de acceso}
+var dirreccion = 'https://serviciovisionnorte.cognitiveservices.azure.com/vision/v3.0/describe?maxCandidates=1';
+
+
--- Pruevas/gato/js/gato.js
+++ Pruevas/gato/js/gato.js
@@ -1,20 +1,17 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function cat() {
-    try 
-    {
-      const response = await axios.get('http://aws.random.cat/meow');
-      igualar(response);
-    } catch (error) 
-    {
-      console.error(error);
-    }
+  try {
+    const response = await axios.get("http://aws.random.cat/meow");
+    igualar(response);
+  } catch (error) {
+    console.error(error);
   }
+}
 
 cat();
 
-function igualar(gato)
-{
-    var cat = gato.data;
-    console.log(cat);
+function igualar(gato) {
+  var cat = gato.data;
+  console.log(cat);
 }
